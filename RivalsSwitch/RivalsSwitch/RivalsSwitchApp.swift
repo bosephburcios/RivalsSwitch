cecb7450fc17ffd0b27ae3d24c0b960cd565ff61
Carlos Olvera <carlosolvera539@gmail.com>
2026-02-16 16:59:52 -0600

    initial commit

---
+++ RivalsSwitch/RivalsSwitch/RivalsSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RivalsSwitchApp.swift
+//  RivalsSwitch
+//
+//  Created by Joseph on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct RivalsSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
